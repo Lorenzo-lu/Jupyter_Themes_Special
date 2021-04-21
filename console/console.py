a8384577087991a3457e396a93968b9c70b4f233
Yizhou Lu <ylu289@wisc.edu>
2021-04-20 19:19:10 -0500

    add cosmo theme

--- console/console.py
+++ console/console.py
@@ -5,13 +5,13 @@ from css_generation import css_generation;
 
 try:
     import subprocess as sp;
-    PATH = sp.getoutput('jupyter --config');
+    path = sp.getoutput('jupyter --config');
 except:
     print('Please install subprocess module first.\nhttps://pypi.org/project/subprocess.run/');
     exit();
 # this is the path to your '.jupyter' folder (if the code above doesn't work)
 #PATH = '/Users/yizhoulu/.jupyter';
-print("Your jupyter config dir is %s"%PATH);
+print("Your jupyter config dir is %s"%path);
 
 
 theme = 'Tiger_theme';
@@ -23,7 +23,7 @@ markdown_line_height = '150%';
 
 # generating the jupyter style:
 # if you see 'Done!', refresh your jupyter
-css_generation(PATH, theme, markdown_size, markdown_line_height,
+css_generation(path, theme, markdown_size, markdown_line_height,
      code_size, code_line_height);
 
 
