e4f769c899eaf7857d64bb5973496240971a407e
Yizhou Lu <ylu289@wisc.edu>
2021-06-18 14:17:46 -0500

    make dict to track the path

--- console/console.py
+++ console/console.py
@@ -8,10 +8,17 @@ path = File.read()[:-1]; ## drop the /n in the file
 File.close();
 print("Your jupyter config dir is %s"%path);
 
+theme_path = {
+    'Tiger': 'Tiger_theme',
+    'Cosmo': 'Cosmo_theme',
+    'Bayern': 'Sports_team_themes/Soccer/Bayern Munchen',
+    };
 
-theme = 'Tiger_theme';
+
+theme = 'Tiger';
 #theme = 'default';
 #theme = 'Cosmo_theme';
+theme = 'Bayern';
 code_size = '11px';                 
 code_line_height = '140%';
 markdown_size = '13px';
@@ -19,7 +26,7 @@ markdown_line_height = '150%';
 
 # generating the jupyter style:
 # if you see 'Done!', refresh your jupyter
-css_generation(path, theme, markdown_size, markdown_line_height,
+css_generation(path, theme_path[theme], markdown_size, markdown_line_height,
      code_size, code_line_height);
 
 
--- console/css_generation.py
+++ console/css_generation.py
@@ -2,20 +2,21 @@ import os;
 import shutil;
 def css_generation(PATH, theme, markdown_size, markdown_line_height,
                    code_size, code_line_height):
+
+    # theme will be the path of theme from a dictionary in this version
     code_path = os.getcwd();
     theme = 'default' if not theme else theme;
     markdown_size = 'none' if not markdown_size else markdown_size;
     markdown_line_height = 'none' if not markdown_line_height else markdown_line_height;
     code_size = 'none' if not code_size else code_size;
     code_line_height = 'none' if not code_line_height else code_line_height;
-    
 
     os.chdir('themes');
     if 'custom' in os.listdir():
         shutil.rmtree('custom');
     os.mkdir('custom');
     
-    if theme in os.listdir():
+    try:
         os.chdir(theme);
         for item in os.listdir():
             src = '%s/themes/%s/%s'%(code_path, theme, item);
@@ -26,6 +27,10 @@ def css_generation(PATH, theme, markdown_size, markdown_line_height,
                 shutil.copy(src, tgt);
         #file = open('custom.css', 'r');
         #css = file.read();
+    except:
+        os.chdir(PATH);
+        print('Wrong theme path');
+        return False;
 
     os.chdir('%s/themes/custom'%(code_path));
     if 'custom.css' in os.listdir():
