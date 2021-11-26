21eb1a00b7277ca293bd06ad85d81b00fd99d54f
Yizhou Lu <ylu289@wisc.edu>
2021-11-26 00:19:31 -0600

    underscore rather than dash for KaTeX-Main

--- console/css_generation.py
+++ console/css_generation.py
@@ -92,7 +92,7 @@ def css_generation(PATH, theme_key, theme, markdown_size, markdown_line_height,
     '''
 
     ## using KaTeX font
-    markdown_font = "KaTeX-Main"; 
+    markdown_font = "KaTeX_Main"; 
     #code_font = "consolas_code"
     code_font = 'UbuntuMono_code'; 
     
